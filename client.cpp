6eaf9cc3cf412e9a355a6edfc50d8c3add546685
John R. Patek <johnpatek2@gmail.com>
2023-12-15 23:46:57 -0600

    updated submodules

--- client.cpp
+++ client.cpp
@@ -1,6 +1,80 @@
+#include <SDL3/SDL.h>
 #include <netput.hpp>
+#include <sigfn.hpp>
 
-int main(int arg, char ** argv)
+#include <iostream>
+
+static void send_keyboard_event(std::unique_ptr<netput::client> &client, const SDL_KeyboardEvent *keyboard)
 {
-
+    const netput::input_state key_state = (keyboard->state == SDL_RELEASED) ? netput::input_state::pressed : netput::input_state::released;
+    client->send_keyboard(keyboard->timestamp, keyboard->windowID, key_state, keyboard->repeat > 0, static_cast<uint32_t>(keyboard->keysym.scancode));
 }
+
+static void client_main()
+{
+    std::unique_ptr<netput::client> client;
+    sigfn::handler_function signal_handler;
+    std::unique_ptr<SDL_Window, std::function<void(SDL_Window *)>> window;
+    bool loop;
+    SDL_Event event;
+
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) < 0)
+    {
+        throw std::runtime_error("SDL_Init: " + std::string(SDL_GetError()));
+    }
+
+    signal_handler = [&](int signum)
+    {
+        std::cerr << "received signal: " << signum << std::endl;
+        loop = false;
+    };
+
+    sigfn::handle(SIGINT, signal_handler);
+    sigfn::handle(SIGTERM, signal_handler);
+
+    window = std::unique_ptr<SDL_Window, std::function<void(SDL_Window *)>>(
+        SDL_CreateWindow("netput!", 640, 480, 0),
+        [](SDL_Window *window)
+        {
+            SDL_DestroyWindow(window);
+        });
+
+    loop = true;
+    while (loop)
+    {
+        if (SDL_PollEvent(&event) == SDL_TRUE)
+        {
+            if (event.type == SDL_EVENT_KEY_UP || event.type == SDL_EVENT_KEY_DOWN)
+            {
+                send_keyboard_event(client, &event.key);
+            }
+
+            if (event.type == SDL_EVENT_QUIT)
+            {
+                std::cerr << "breaking loop." << std::endl;
+                loop = false;
+            }
+        }
+    }
+}
+
+int main(int arg, char **argv)
+{
+    int status;
+
+    status = 0;
+
+    try
+    {
+        client_main();
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+        status = 1;
+    }
+    SDL_Quit();
+    return status;
+}
+
+static void client_main();
--- server.cpp
+++ server.cpp
@@ -1,5 +1,57 @@
+#include <netput.hpp>
+#include <sigfn.hpp>
 
-int main(int arg, char ** argv)
+#include <iostream>
+
+static void server_main()
 {
-    
+    std::unique_ptr<netput::server> server;
+    sigfn::handler_function signal_handler;
+    server = std::unique_ptr<netput::server>(new netput::server("0.0.0.0", 12345));
+
+    signal_handler = [&](int signum)
+    {
+        server->shutdown();
+    };
+
+    server->handle_keyboard(
+        [&](
+            const std::string &session_id,
+            uint64_t timestamp,
+            uint32_t window_id,
+            netput::input_state state,
+            bool repeat,
+            uint32_t key_code)
+        {
+            std::string state_string("released");
+            if (!repeat)
+            {
+                if (state == netput::input_state::pressed)
+                {
+                    state_string = "pressed";
+                }
+                std::cerr << "key " << key_code << " " << state_string << std::endl;
+            }
+        });
+
+    server->serve();
+}
+
+int main(int arg, char **argv)
+{
+    int status;
+
+    status = 0;
+
+    try
+    {
+        server_main();
+    }
+    catch (const std::exception &exception)
+    {
+        std::cerr << exception.what() << std::endl;
+        status = 1;
+    }
+
+    return status;
 }
